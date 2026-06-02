18a7db19e882a5f06b61381917f99c7b6360ef0a
siddhikadongare98-code <siddhikadongare98@gmail.com>
2026-06-02 11:36:54 +0530

    Update 60-siddhika-dongare.py

--- lab/60-siddhika-dongare.py
+++ lab/60-siddhika-dongare.py
@@ -4,3 +4,17 @@ while n<=10:
   t+=n
   n+=1
 print(t)
+#problem2
+a=4
+b==9
+c=19
+def even_odd(n)
+  if n % 2 == 0:
+    return "Even"
+  else:
+    return "odd"
+    print(even_odd(a))
+    print(even_odd(b))
+    print(even_odd((c))
+    
+
