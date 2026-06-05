21f3627c17e1b9b4b9302bfc77e2ba427418a3bf
ayushkamutdak-creator <ayushkamutdak@gmail.com>
2026-06-05 11:32:03 +0530

    Update Aayushka-f2.py

--- students/aayushka-mutdak/Aayushka-f2.py
+++ students/aayushka-mutdak/Aayushka-f2.py
@@ -1,1 +1,15 @@
+#problem 1
+s={'a','b','c','d'}
+s.update({'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','y','z'})
+print(s)
+count=0
+count= 26
+vowels ={'a','e','i', 'o', 'u'}
+
+for x in s:
+     if x in vowels:
+        count = count - 1
+
+print("Vowel count:", count)
+
 
