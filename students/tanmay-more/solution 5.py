c6e67137380c297b17a3c4b15653b5ab9bb06704
tanmaymore1705-dotcom <tanmaymore1705@gmail.com>
2026-06-10 11:17:12 +0530

    range
    Added functionality to input student names and ages into a dictionary.

--- students/tanmay-more/solution 5.py
+++ students/tanmay-more/solution 5.py
@@ -20,3 +20,15 @@ student = input_students(50)
 print(student)
 
 #problem no 4
+m = int(input("No of members: "))
+
+d = {}
+
+for x in range(m):
+    name = input(f"Enter the name of student {x+1}: ")
+    age = int(input("Enter age: "))
+    
+    d[name] = age
+
+print("Student Details:")
+print(d)
