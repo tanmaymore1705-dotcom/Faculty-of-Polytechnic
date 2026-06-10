f5089320232061ed2532d34606303c4a35a971d1
tanmay <tanmaymore1705@gmail.com>
2026-06-10 18:29:39 +0530

    Correct indentation and input prompt in solution6.py
    Fixed indentation issues and improved input prompt.

--- students/tanmay-more/solution6.py
+++ students/tanmay-more/solution6.py
@@ -1,19 +1,20 @@
 #problem no 1
-num = 10 
+num = 10
+
 while True:
-    guess = int(input("Guess a number between 1 and 20: "))
+    guess = int(input("Guess a number between 1 and 20:"))
+
     if guess == num:
         print("Right")
-        break  # FIX(faculty): added break so the loop stops on a correct guess (was an infinite loop)
+        
     else:
         print("Wrong")
-
 #problem no 2
-num = 10  # FIX(faculty): dedented to column 0 (leading spaces caused an IndentationError)
+num = 10
 
 while True:
     guess = int(input("Guess a number between 1 and 20: "))
-    
+
     if guess == num:
         print("Right")
         break
