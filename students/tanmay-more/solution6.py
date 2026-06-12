b31bb79e34a33a031d587435f0f3ce5c09ab7cb5
tanmay <tanmaymore1705@gmail.com>
2026-06-12 22:01:39 +0530

    Refactor guessing game logic in solution6.py
    Removed redundant guessing logic and fixed indentation.

--- students/tanmay-more/solution6.py
+++ students/tanmay-more/solution6.py
@@ -6,17 +6,6 @@ while True:
 
     if guess == num:
         print("Right")
-        
-    else:
-        print("Wrong")
-#problem no 2
-num = 10
-
-while True:
-    guess = int(input("Guess a number between 1 and 20: "))
-
-    if guess == num:
-        print("Right")
         break
     else:
         print("Wrong")
