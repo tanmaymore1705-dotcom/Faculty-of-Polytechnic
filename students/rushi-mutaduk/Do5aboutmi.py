9ac7afe4e6d7e581f009242e3584b2971cd895c3
mutdakrushi-png <mutdakrushi@gmail.com>
2026-06-16 18:12:26 +0530

    Create Do5aboutmi.py (#310)

---
+++ students/rushi-mutaduk/Do5aboutmi.py
@@ -0,0 +1,10 @@
+name=input("What is your name? --> ")
+while True:
+    name=input("What is your name?-->")
+    if name!="":
+        break
+    else:
+        print("name cant be empty")
+age=int(input("How old are you? --> "))
+city=input("Which city are you from? --> ")
+language=input("Which one your favourite programing language? --> ")
