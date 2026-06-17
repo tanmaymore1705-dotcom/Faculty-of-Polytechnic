af8e0a726bbc089a81a8f5fd79be1e8b8652f64b
Priya <priyapunde543@gmail.com>
2026-06-17 10:27:11 +0530

    Update Cp1.py

--- students/priya-punde/Cp1.py
+++ students/priya-punde/Cp1.py
@@ -2,3 +2,16 @@
 def avg(a,b,c):
     a=(a,b,c)/3
     return(a)
+#problem2
+def l_g(avg):
+    if avg>=90:
+        return"A"
+    elif avg>=80:
+        return"B"
+    elif avg>70:
+        return"C"
+    elif avg>=60:
+        return"D"
+    else:
+        return"F"
+print(l_g(35))
