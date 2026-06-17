a3dde276bfeb62de953ecea0ab499ed2ce72e64e
Priya <priyapunde543@gmail.com>
2026-06-17 10:33:13 +0530

    Update Cp1.py

--- students/priya-punde/Cp1.py
+++ students/priya-punde/Cp1.py
@@ -19,3 +19,13 @@ print(l_g(35))
 def fo_ma(m1,m2,m3):
     return(f"{m1},{m2},{m3}")
 print(fo_ma(20,4,5))
+#problem4
+
+def classify_result(avg):
+    if avg>=75:
+        print("disc")
+    elif avg>=35:
+        print("pass")
+    else:
+        print("fail")
+classify_result(56)
